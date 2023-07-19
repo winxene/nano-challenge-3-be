b3f018da3130e61f1b70cfc9fcc5a447eb1ad42e
winxene <winxeneric@gmail.com>
2023-07-19 16:10:30 +0700

    feat: added dummy admin detail data and revised user detail data

--- src/utils/dummyData.js
+++ src/utils/dummyData.js
@@ -1,72 +1,100 @@
+//note: the latitude range is between -6.2449373429657244 to -6.24356169422578
+// the longitude range is between 106.79798496354378 to 106.79824773542873
+
+// if the code fully works, delete this dummy report
 let dummyReport = [
   {
-    id: 1,
-    GeolocationCoordinates: {
+    userID: 1,
+    geolocationCoordinates: {
       latitude: -6.244932845946446,
       longitude: 106.79802070826771,
     },
-    pinType: "lansia",
   },
   {
-    id: 1,
-    GeolocationCoordinates: {
+    userID: 1,
+    geolocationCoordinates: {
       latitude: -6.2449373429657244,
       longitude: 106.79821898654653,
     },
-    pinType: "lansia",
   },
   {
-    id: 1,
-    GeolocationCoordinates: {
+    userID: 1,
+    geolocationCoordinates: {
       latitude: -6.24356169422578,
       longitude: 106.79824773542873,
     },
-    pinType: "lansia",
   },
   {
-    id: 1,
-    GeolocationCoordinates: {
+    userID: 1,
+    geolocationCoordinates: {
       latitude: -6.243745787357868,
       longitude: 106.79798496354378,
     },
-    pinType: "lansia",
   },
 ];
 
 //dummy detail data for testing in JSON format
 let dummyDetailData = [
   {
-    id: 1,
+    userID: 1,
     name: "Asep",
+    email: "asep@email.com",
+    password: "asep123",
+    pinType: "lansia",
     description: "Asep is Lansia",
-    image: "https://via.placeholder.com/150",
+    location: "Concourse",
+    status: "assisted", //assisted, requesting, not available
+    userImage: "https://via.placeholder.com/150",
+    locationImage: "https://via.placeholder.com/150",
+    geolocationCoordinates: {
+      latitude: -6.243745787357868,
+      longitude: 106.79798496354378,
+    },
   },
   {
-    id: 2,
-    name: "Boodi",
-    description: "Boodi is Disabled",
-    image: "https://via.placeholder.com/150",
+    userID: 2,
+    name: "Tina",
+    email: "tina@email.com",
+    password: "tina123",
+    pinType: "ibu hamil",
+    description: "Tina is Pregnant",
+    location: "Toilet",
+    isHelped: "requesting", //assisted, requesting, not available
+    userImage: "https://via.placeholder.com/150",
+    locationImage: "https://via.placeholder.com/150",
+    geolocationCoordinates: {
+      latitude: -6.2449373429657244,
+      longitude: 106.79821898654653,
+    },
   },
 ];
 
-//dummy notification data for testing in JSON format
-let dummyNotificationsData = [
+//dummy detail data for testing in JSON format
+let dummyAdminDetailData = [
   {
-    title: "Asep needs help",
-    description: "Asep needs help near toilet at MRT Dukuh Atas",
+    adminID: 1,
+    name: "Admin 1",
+    email: "admin1@email.com",
+    password: "admin123",
+    status: "available", //available, on duty, not available
+    adminImage: "https://via.placeholder.com/150",
   },
   {
-    title: "Asep needs help",
-    description: "Asep needs help near toilet at MRT Dukuh Atas",
+    adminID: 2,
+    name: "Admin 2",
+    email: "admin2@email.com",
+    password: "admin123",
+    status: "available", //available, on duty, not available
+    adminImage: "https://via.placeholder.com/150",
   },
 ];
 
-const getDummyDetailData = () => dummyDetailData;
+const getDummyUserDetailData = () => dummyDetailData;
+const getDummyAdminDetailData = () => dummyAdminDetailData;
 const getDummyReportData = () => dummyReport;
-const getDummyNotificationData = () => dummyNotificationsData;
 
 module.exports = {
-  getDummyDetailData,
+  getDummyUserDetailData,
   getDummyReportData,
-  getDummyNotificationData,
+  getDummyAdminDetailData,
 };
