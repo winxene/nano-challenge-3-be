abd81079e36797f62395179ec8f8ce5ed5fc2db8
winxene <winxeneric@gmail.com>
2023-07-20 15:39:39 +0700

    chore: renamed success message in console log

--- src/api/controllers/adminDetailController.js
+++ src/api/controllers/adminDetailController.js
@@ -3,7 +3,7 @@ const { getDummyAdminDetailData } = require("../../utils/dummyData");
 const getReportAdminDetailData = (req, res) => {
   const dummyDetailData = getDummyAdminDetailData();
   res.json(dummyDetailData);
-  console.log("Succeeded get report detail data");
+  console.log("Succeeded get admin detail data");
 };
 
 module.exports = {
--- src/api/controllers/userDetailController.js
+++ src/api/controllers/userDetailController.js
@@ -3,7 +3,7 @@ const { getDummyUserDetailData } = require("../../utils/dummyData");
 const getReportUserDetailData = (req, res) => {
   const dummyDetailData = getDummyUserDetailData();
   res.json(dummyDetailData);
-  console.log("Succeeded get report detail data");
+  console.log("Succeeded get user detail data");
 };
 
 module.exports = {
