74e46faa5bf4302fd54281ea1e0d7bacdc17b11b
winxene <winxeneric@gmail.com>
2023-07-21 18:04:21 +0700

    refactor: changed the folder structure and the way admin detail and user detail crud by ID

--- src/api/routes/details/adminDetails.js
+++ src/api/routes/details/adminDetails.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getReportAdminDetailData,
+  getSpecificAdminDetailData,
 } = require("../../controllers/details/adminDetailController");
 
-router.get("/", getReportAdminDetailData);
+router.get("/", (req, res) => {
+  res.status(400).json({ error: "adminID parameter is required." });
+});
+
+// Define the route handler for "/user-detail/:adminID"
+router.get("/:adminID", getSpecificAdminDetailData);
 
 module.exports = router;
--- src/api/routes/details/allUserDetails.js
+++ src/api/routes/details/allUserDetails.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {
   getReportUserDetailData,
-} = require("../../controllers/details/userDetailController");
+} = require("../../controllers/details/allUserDetailController");
 
 router.get("/", getReportUserDetailData);
 
