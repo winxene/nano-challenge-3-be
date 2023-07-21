7049e2cc90a00bbf4216a04286e7b4997383c182
winxene <winxeneric@gmail.com>
2023-07-21 18:25:46 +0700

    feat: added http post for restarting the server

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { spawn } = require("child_process");
 const app = express();
 const http = require("http");
 const socketIO = require("socket.io");
@@ -32,6 +33,19 @@ app.use("/user-detail", userDetailRoutes);
 app.use("/admin-detail", adminDetailRoutes);
 app.use("/auth/user", userAuthRoutes);
 app.use("/auth/admin", adminAuthRoutes);
+
+//for debugging purpose only
+app.post("/restart", (req, res) => {
+  // Restart the server using the 'spawn' method
+  const restartCommand = process.platform === "win32" ? "npm.cmd" : "npm";
+  const args = ["run", "start"]; // Change "start" to the actual script name if different
+  const options = { detached: true, stdio: "ignore" };
+  const restartProcess = spawn(restartCommand, args, options);
+  restartProcess.unref();
+
+  // Send a response indicating the server is restarting
+  res.status(200).send("Server is restarting...");
+});
 
 // Socket.io configuration and event handlers
 io.on("connection", (socket) => {
